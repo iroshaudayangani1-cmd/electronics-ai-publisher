f587fa273a283abf0d76500663e96af0189f6b91
iroshaudayangani1-cmd <iroshaudayangani1@gmail.com>
2026-08-02 14:33:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
-
 from core.news_collector import collect_news
+
 from core.article_rewriter import rewrite_articles
+
 from core.image_generator import generate_images
+
 from core.publisher import publish_articles
 
 
 def main():
 
-    print("===== ELECTRONICS AI PUBLISHER =====")
+    print("=" * 60)
+    print("EDATA SL AUTOMATION")
+    print("=" * 60)
 
-    print("\nStep 1 : Collecting Articles")
+    print("\nStep 1 : Collecting Electronics News")
+
     collect_news()
 
-    print("\nStep 2 : Rewriting")
+    print("\nStep 2 : Rewriting Article")
+
     rewrite_articles()
 
-    print("\nStep 3 : Creating Images")
+    print("\nStep 3 : Generating AI Image")
+
     generate_images()
 
     print("\nStep 4 : Publishing")
+
     publish_articles()
 
-    print("\nDone.")
+    print("\nAll Tasks Completed Successfully")
 
 
 if __name__ == "__main__":
+
     main()
