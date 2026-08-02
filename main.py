0a7aa03f6c187d6b5e95237a9282ead6b9875844
iroshaudayangani1-cmd <iroshaudayangani1@gmail.com>
2026-08-02 13:34:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 
+from core.news_collector import collect_news
+from core.article_rewriter import rewrite_articles
+from core.image_generator import generate_images
+from core.publisher import publish_articles
+
+
+def main():
+
+    print("===== ELECTRONICS AI PUBLISHER =====")
+
+    print("\nStep 1 : Collecting Articles")
+    collect_news()
+
+    print("\nStep 2 : Rewriting")
+    rewrite_articles()
+
+    print("\nStep 3 : Creating Images")
+    generate_images()
+
+    print("\nStep 4 : Publishing")
+    publish_articles()
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
