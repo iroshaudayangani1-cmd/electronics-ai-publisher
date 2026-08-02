4083a2ad4e00db721fd7e130884f601fba017a1e
iroshaudayangani1-cmd <iroshaudayangani1@gmail.com>
2026-08-02 15:11:14 +0530

    Update cloudinary_uploader.py

--- core/cloudinary_uploader.py
+++ core/cloudinary_uploader.py
@@ -1,1 +1,63 @@
 
+import os
+
+import cloudinary
+import cloudinary.uploader
+
+from config.settings import (
+    CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET,
+)
+
+
+# ==========================================================
+# CLOUDINARY CONFIG
+# ==========================================================
+
+cloudinary.config(
+
+    cloud_name=CLOUDINARY_CLOUD_NAME,
+
+    api_key=CLOUDINARY_API_KEY,
+
+    api_secret=CLOUDINARY_API_SECRET,
+
+    secure=True,
+
+)
+
+
+# ==========================================================
+# UPLOAD IMAGE
+# ==========================================================
+
+def upload_image(image_path):
+
+    if not os.path.exists(image_path):
+
+        raise FileNotFoundError(image_path)
+
+    try:
+
+        result = cloudinary.uploader.upload(
+
+            image_path,
+
+            folder="edata-sl",
+
+            resource_type="image",
+
+            overwrite=True,
+
+        )
+
+        return result["secure_url"]
+
+    except Exception as e:
+
+        print("Cloudinary Upload Error")
+
+        print(e)
+
+        raise
